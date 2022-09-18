2c62e39d0a1ef5efec352dc75a365c55257bb089
MatejPemper <90246393+MatejPemper@users.noreply.github.com>
2022-09-18 17:36:18 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InicijalnoPonavljanje6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int i = 1;
+            Console.WriteLine("Unesi broj članova reda za sumu: ");
+            int brClanova = Convert.ToInt32(Console.ReadLine());
+
+            do
+            {
+                Console.Write(" + 1/" + Math.Pow(3, i));
+                i++;
+            }
+            while (i <= brClanova);
+
+
+
+                Console.ReadKey();
+        }
+    }
+}
